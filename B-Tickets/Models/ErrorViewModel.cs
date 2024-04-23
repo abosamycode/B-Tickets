f6eea014f80cd8a62d9b6a78129153f837b92900
moo <abosamys145@gmail.com>
2024-04-23 15:12:47 +0200

    Add project files.

---
+++ B-Tickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B_Tickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
